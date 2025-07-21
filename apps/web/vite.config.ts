7fe3409ff496d72c89e52191d49fbc74423f9818
loclv <masamonedante@gmail.com>
2025-07-21 09:01:23 +0700

    refactor: biome config, format some files

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,35 +1,35 @@
-import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    TanStackRouterVite({}),
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "truyGoc",
-        short_name: "truyGoc",
-        description: "truyGoc - PWA Application",
-        theme_color: "#0c0c0c",
-      },
-      pwaAssets: {
-        disabled: false,
-        config: true,
-      },
-      devOptions: {
-        enabled: true,
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [
+		tailwindcss(),
+		TanStackRouterVite({}),
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				name: "truyGoc",
+				short_name: "truyGoc",
+				description: "truyGoc - PWA Application",
+				theme_color: "#0c0c0c",
+			},
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
+			devOptions: {
+				enabled: true,
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
